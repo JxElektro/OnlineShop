a98210065da00fa7a70c79975628e0efb13b0901
Jhen Nunez <jxelektro@gmail.com>
2022-10-01 14:10:44 -0300

    _

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import  { CartProvider }  from  './Context/CartContext' ;
 import './App.css';
 import NavBar from './Components/NavBar';
-
+import Footer from './Components/Footer';
 
 // Componente principal
 function App() {
   return (
     <CartProvider>
     <NavBar />
+    <Footer />
     </CartProvider>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// This Footer component will show some contact information
+// It is used in the App.js file
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h1>Info</h1>
+    </div>
+  );
+}
